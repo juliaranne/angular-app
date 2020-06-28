1ed8b6a95c15916cec6acef58b67d43a4a6d63f5
Julia Robinson <juliaarobinson@hotmail.com>
2020-06-28 22:53:47 +0100

    working custom validator

--- src/app/shared/name-input-validator.directive.ts
+++ src/app/shared/name-input-validator.directive.ts
@@ -1,27 +1,24 @@
-import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
+import { AsyncValidator, AbstractControl, NG_ASYNC_VALIDATORS, Validator, ValidationErrors } from '@angular/forms';
 import { Directive } from '@angular/core';
 import { PlayerService } from './player.service';
+import {Observable} from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Directive({
   selector: '[appInputValidator]',
   providers: [{
-    provide: NG_VALIDATORS,
+    provide: NG_ASYNC_VALIDATORS,
     useExisting: inputNameValidatorDirective,
     multi: true
   }]
 })
-export class inputNameValidatorDirective implements Validator {
+export class inputNameValidatorDirective implements AsyncValidator {
 
   constructor(private playerService: PlayerService) { }
 
-  validate(control: AbstractControl): {[key: string]: any} | null {
-    this.playerService.checkIfNameExists(control.value)
-    .subscribe(response => {
-      if (response.length !== 0) {
-        return { 'nameTaken': 'Name already taken' }
-      }
-      return null;
-    });
-    return null;
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    return this.playerService.checkIfNameExists(control.value).pipe(
+      map((result) => result.length ? {nameTaken: 'already taken'} : null)
+    );
   }
 }
